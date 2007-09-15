73d1b1ad6d2902f2af0790049df2f76f210b97bc
Roy Ivy III <rivy@cpan.org>
2007-09-16 01:44:32 +0500

    Developement commit

--- lib/Win32/CommandLine.pm
+++ lib/Win32/CommandLine.pm
@@ -7,7 +7,7 @@ use warnings;
 
 # VERSION: x.y[.date[.build]]  { y is odd = beta/experimental; y is even = release }
 #use version qw(); our $VERSION = version::qv('0.1'.'.'.join(q{}, split(/\//xms,qw($Date$)[-3])).q{.}.qw($Rev$)[-2]);		## no critic ( ProhibitCallsToUnexportedSubs )
-use version qw(); our $VERSION = version::qv(qw($Version$)[-2]);		## no critic ( ProhibitCallsToUnexportedSubs )
+use version qw(); our $VERSION = version::qv(qw($Version 1 $)[-2]);		## no critic ( ProhibitCallsToUnexportedSubs )
 
 # Module Summary
 
